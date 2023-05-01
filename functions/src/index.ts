4a35696f7698d8e13f9ca7b6781cd14272d150fc
Hideo Yasumoto <hideo54@hideo54.com>
2023-05-01 13:51:34 +0900

    Create https function using fastify

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,23 @@
 import * as functions from 'firebase-functions';
+import Fastify from 'fastify';
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-    functions.logger.info('Hello logs!', { structuredData: true });
-    response.send('Hello from Firebase!');
+const fastify = Fastify();
+
+// https://www.fastify.io/docs/latest/Guides/Serverless/#add-custom-contenttypeparser-to-fastify-instance
+fastify.addContentTypeParser('application/json', {}, (_, body, done) => {
+    done(null, body);
 });
+
+fastify.get('/hello', (_, reply) => {
+    reply.send({
+        message: 'Hello, world!',
+    });
+});
+
+// https://www.fastify.io/docs/latest/Guides/Serverless/#implement-and-export-the-function
+const app = async (req: functions.Request, res: functions.Response) => {
+    await fastify.ready();
+    fastify.server.emit('request', req, res);
+};
+
+export const hideo54Api = functions.region('asia-northeast1').https.onRequest(app);
