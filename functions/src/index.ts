b03feef42479be6ac409963a9346205e1bd2aa2c
Hideo Yasumoto <hideo54@hideo54.com>
2023-05-01 14:54:41 +0900

    Add jp-local-gov endpoint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,6 @@
 import * as functions from 'firebase-functions';
 import Fastify from 'fastify';
+import { searchCodeInfo } from 'jp-local-gov';
 
 const fastify = Fastify();
 
@@ -12,6 +13,38 @@ fastify.get('/hello', (_, reply) => {
     reply.send({
         message: 'Hello, world!',
     });
+});
+
+fastify.get<{
+    Querystring: {
+        q?: string;
+    };
+}>('/jp-local-gov/search-code', (req, reply) => {
+    const searchWord = req.query.q;
+    if (searchWord) {
+        const result = searchCodeInfo(searchWord);
+        reply.send({
+            result,
+        });
+    } else {
+        reply.code(400).send({
+            error: true,
+        });
+    }
+});
+
+fastify.get<{
+    Querystring: {
+        q?: string;
+    };
+}>('/jp-local-gov/search-code-for-excel', (req, reply) => {
+    const searchWord = req.query.q;
+    const firstHitCode = searchWord && searchCodeInfo(searchWord)[0]?.code;
+    if (firstHitCode) {
+        reply.send(firstHitCode);
+    } else {
+        reply.code(400).send();
+    }
 });
 
 // https://www.fastify.io/docs/latest/Guides/Serverless/#implement-and-export-the-function
